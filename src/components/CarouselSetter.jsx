a564b2c0ed38060dc6fd63511e5940edd4f373e1
KingsukNandi <kingsuk24nandi@gmail.com>
2024-07-09 20:46:59 +0530

    Fix editor window width problem

--- src/components/CarouselSetter.jsx
+++ src/components/CarouselSetter.jsx
@@ -13,20 +13,15 @@ function CarouselSetter() {
   }, []);
 
   if (windowSize > 900) {
+    return <Editor />;
+  } else {
     return (
-      <div className="flex justify-evenly items-center gap-3">
-        <Editor />
-      </div>
-    );
-  }
-  return (
-    <>
-      <div className="flex justify-evenly items-center gap-3">
+      <>
         <Carousel scrollDistance={300} showArrows="hover">
           <Editor />
         </Carousel>
-      </div>
-    </>
-  );
+      </>
+    );
+  }
 }
 export default CarouselSetter;
--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -5,20 +5,20 @@ import CodeEditorHTML from "./CodeEditorHTML";
 
 function Editor() {
   return (
-    <>
-      <div className={`w-1/3`}>
+    <div className="flex justify-evenly items-center gap-3 w-fit md:w-full">
+      <div className="w-72 md:w-1/3">
         <span className="texts">HTML</span>
         <CodeEditorHTML />
       </div>
-      <div className="w-1/3">
+      <div className="w-72 md:w-1/3">
         <span className="texts">CSS</span>
         <CodeEditorCSS />
       </div>
-      <div className="w-1/3">
+      <div className="w-72 md:w-1/3">
         <span className="texts">JS</span>
         <CodeEditorJS />
       </div>
-    </>
+    </div>
   );
 }
 
