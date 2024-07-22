ff9936a53bb73d75fd657f6e2cde24de19b5a853
KingsukNandi <kingsuk24nandi@gmail.com>
2024-07-22 20:25:40 +0530

    Update placeholder texts for editors

--- src/store/codeSlice.js
+++ src/store/codeSlice.js
@@ -1,40 +1,9 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  htmlCode: `<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-</head>
-<body>
-  <button id="click-me">Click Count : 0</button>
-</body>
-</html>`,
-  cssCode: `body {
-  background-color: #282c34;
-  color: #fff;
-}
-#click-me {
-  background-color: #3d4148;
-  color: #fff;
-  border: none;
-  padding: 10px 20px;
-  border-radius: 5px;
-  cursor: pointer;
-  transition: all 0.3s;
-}
-#click-me:hover {
-  background-color: #52565c;
-}`,
-  jsCode: `let clickCount = 0;
-document
-  .getElementById("click-me")
-  .addEventListener("click", function() {
-    clickCount++;
-    this.textContent = \`Click Count : \${clickCount}\`;
-  });`,
+  htmlCode: `<!-- Type HTML code here -->`,
+  cssCode: `/* Type CSS here */`,
+  jsCode: `// Type JavaScript code here`,
 };
 export const codeSlice = createSlice({
   name: "code",
